1096d7b7c24509640e7a521064f140334d1b082d
marie <marie.blanchet.pro@gmail.com>
2020-06-16 19:07:00 +0200

    add fr translations for errors ans activerecord

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,8 @@ module GermainPortfolio
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
     # Permitted locales available for the application
-    I18n.available_locales = [:en, :fr, :de]
     config.i18n.default_locale = :fr
   end
 end

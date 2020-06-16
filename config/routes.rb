c51cfe221b914c8bd5a09643b3ed4ff0a455f487
marie <marie.blanchet.pro@gmail.com>
2020-06-16 21:29:24 +0200

    configure locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'pictures/index'
-  get 'pictures/show'
-  get 'repertories/index'
-  get 'repertories/show'
-  root to: 'extracts#index'
+
+  scope '(:locale)', locale: /en|de/ do
+    root to: 'pages#home'
+    # resources :articles
+    get 'articles/index'
+    get 'articles/show'
+    get 'pictures/index'
+    get 'pictures/show'
+    get 'repertories/index'
+    get 'repertories/show'
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
